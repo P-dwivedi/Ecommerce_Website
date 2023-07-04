611d60de9491107f51347a7602ba953287313884
P-dwivedi <130329992+P-dwivedi@users.noreply.github.com>
2023-07-04 17:34:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multi", {
+  strings: [
+    "NOW AVAILABLE AT OUR BESCO STORE!",
+    "NOW AVAILABLE AT OUR BESCO STORE!",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
